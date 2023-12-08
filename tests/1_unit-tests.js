24f62d8c71563faa0e51c120866a9e1d6b0fc6e4
Damilare Elijah ISMAILA <bestbynature2010@gmail.com>
2023-12-08 17:51:07 +0000

    feat: Added or modified assertions
    Added or modified assertions to test various conditions

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -82,17 +82,17 @@ suite("Unit Tests", function () {
   suite("Comparisons", function () {
     // #8
     test("#isAbove, #isAtMost", function () {
-      assert.fail("hello".length, 5);
-      assert.fail(1, 0);
-      assert.fail(Math.PI, 3);
-      assert.fail(1 - Math.random(), 1);
+      assert.isAtMost("hello".length, 5);
+      assert.isAbove(1, 0);
+      assert.isAbove(Math.PI, 3);
+      assert.isAtMost(1 - Math.random(), 1);
     });
     // #9
     test("#isBelow, #isAtLeast", function () {
-      assert.fail("world".length, 5);
-      assert.fail(2 * Math.random(), 0);
-      assert.fail(5 % 2, 2);
-      assert.fail(2 / 3, 1);
+      assert.isAtLeast("world".length, 5);
+      assert.isAtLeast(2 * Math.random(), 0);
+      assert.isBelow(5 % 2, 2);
+      assert.isBelow(2 / 3, 1);
     });
     // #10
     test("#approximately", function () {
